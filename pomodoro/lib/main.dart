ee2e091ec15ef4198bf14719051babc30422635a
0-manbir <0.manbirsingh@gmail.com>
2024-06-20 16:39:50 +0530

    Rewriting code again coz why not

--- pomodoro/lib/main.dart
+++ pomodoro/lib/main.dart
@@ -1,26 +1,9 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
-import 'package:pomodoro/pages/home.dart';
-import 'package:pomodoro/variables/colors.dart';
-import 'package:window_manager/window_manager.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await windowManager.ensureInitialized();
-
-  await windowManager.waitUntilReadyToShow().then((_) async {
-    // await windowManager.setTitleBarStyle(TitleBarStyle.normal);
-
-    await windowManager.setSkipTaskbar(false); // add this when minimised
-    await windowManager.setOpacity(1); // add this when minimised
-    await windowManager.setAlwaysOnTop(false); // add this when minimised
-
-    // await windowManager.setIcon();
-    await windowManager.setTitle("Pomodoro Timer");
-    await windowManager.setMinimumSize(const Size(600, 400));
-    await windowManager.setSize(const Size(1100, 700));
-    await windowManager.show();
-  });
   runApp(const MyApp());
 }
 
@@ -32,21 +15,12 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  // Screen Dimensions
-  late double screenWidth, screenHeight;
-
   @override
   Widget build(BuildContext context) {
-    screenWidth = MediaQuery.of(context).size.width;
-    screenHeight = MediaQuery.of(context).size.height;
-
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Pomodoro Timer',
-      home: Scaffold(
-        backgroundColor: background,
-        body: HomeScreen(),
-      ),
+      home: Scaffold(),
     );
   }
 }
--- pomodoro/lib/variables/colors.dart
+++ pomodoro/lib/variables/colors.dart
@@ -10,3 +10,4 @@ const Color accent = Color.fromRGBO(224, 169, 178, 1);
 
 const Color text = Color.fromRGBO(10, 10, 10, 1);
 const Color background = Color.fromRGBO(247, 249, 248, 1);
+const Color shadow = Color.fromRGBO(0, 0, 0, 0.5);
